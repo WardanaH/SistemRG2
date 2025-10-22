8ec5db4a3f3f9dc11139fef968583997f06d497a
WardanaH <rizkiwardana10@gmail.com>
2025-10-22 12:29:13 +0700

    first Commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h3>Selamat Datang, {{ $user->name }}</h3>
+    <p>Role Anda: <strong>{{ $user->getRoleNames()->implode(', ') }}</strong></p>
+
+    <div class="mt-4">
+        <a href="{{ route('users.index') }}" class="btn btn-outline-primary">Manajemen User</a>
+        <a href="{{ route('cabangs.index') }}" class="btn btn-outline-success">Manajemen Cabang</a>
+        <a href="{{ route('roles.index') }}" class="btn btn-outline-warning">Manajemen Hak Akses</a>
+        <a href="{{ route('managesupplierindex') }}" class="btn btn-outline-warning">Manajemen Supplier</a>
+        <a href="{{ route('managekategoriindex') }}" class="btn btn-outline-warning">Manajemen Kategori</a>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\CabangController;
+use App\Http\Controllers\JenisPelanggansController;
+
+// Guest (belum login)
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+});
+
+// Authenticated (sudah login)
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Manajemen Sistem
+    Route::middleware(['role:admin|direktur|manajemen|supervisor'])->group(function () {
+        Route::resource('users', UserController::class);
+        Route::resource('cabangs', CabangController::class);
+        Route::resource('roles', RoleController::class)->only(['index', 'store', 'update', 'destroy']);
+    });
+});
+
+//supplier
+Route::middleware(['auth'])->group(function () {
+    Route::get('/supplier', [SupplierController::class, 'index'])->name('managesupplierindex');
+    Route::get('/supplier/loadsupplier', [SupplierController::class, 'loadsupplier'])->name('loadsupplier');
+    Route::post('/supplier/postsupplier', [SupplierController::class, 'store'])->name('storesupplier');
+    Route::post('/supplier/updatesupplier', [SupplierController::class, 'update'])->name('updatesupplier');
+    Route::post('/supplier/deletesupplier', [SupplierController::class, 'destroy'])->name('deletesupplier');
+});
+
+//jenis pelanggans
+Route::middleware(['auth'])->group(function () {
+    Route::get('/jenispelanggan', [JenisPelanggansController::class, 'index'])->name('jenispelanggan.index');
+    Route::post('/jenispelanggan/store', [JenisPelanggansController::class, 'store'])->name('jenispelanggan.store');
+    Route::put('/jenispelanggan/update', [JenisPelanggansController::class, 'update'])->name('jenispelanggan.update');
+    Route::delete('/jenispelanggan/destroy', [JenisPelanggansController::class, 'destroy'])->name('jenispelanggan.destroy');
+});
+
+// Kategori
+Route::middleware(['auth'])->group(function () {
+    Route::get('/kategori', [App\Http\Controllers\KategoriController::class, 'index'])->name('managekategoriindex');
+    Route::get('/kategori/loadkategori', [App\Http\Controllers\KategoriController::class, 'loadkategori'])->name('loadkategori');
+    Route::post('/kategori/postkategori', [App\Http\Controllers\KategoriController::class, 'store'])->name('storekategori');
+    Route::post('/kategori/updatekategori', [App\Http\Controllers\KategoriController::class, 'update'])->name('updatekategori');
+    Route::post('/kategori/deletekategori', [App\Http\Controllers\KategoriController::class, 'destroy'])->name('deletekategori');
+});
+
