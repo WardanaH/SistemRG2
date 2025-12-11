0bf3840d0c6d0d1fa61d544c1ac7dd7e9e9550a7
WardanaH <rizkiwardana10@gmail.com>
2025-12-11 15:01:40 +0700

    msaih nyoba ya

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ use App\Http\Controllers\TransaksiPenjualansController;
 require __DIR__ . '/operator.php';
 require __DIR__ . '/designer.php';
 require __DIR__ . '/projects.php';
+require __DIR__ . '/bot.php';
 
 Route::get('/', function () {
     $user = auth()->user();
