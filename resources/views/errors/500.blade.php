c7e5ed7ce9a7add2094223c557824ef5caab9cf2
WardanaH <rizkiwardana10@gmail.com>
2026-01-10 08:39:39 +0700

    update error page
    updating error page with image

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="d-flex flex-column align-items-center justify-content-center mt-5">
+        <img
+            src="{{ asset('images/errors/Error page 500.png') }}"
+            class="img-fluid rounded-top"
+            alt="error-403"
+            style="max-width: 1000px" />
+
+        <div class="mt-3">
+            <a href="{{ route('home') }}" class="btn btn-primary">Kembali ke Beranda</a>
+        </div>
+    </div>
+</body>
+
+</html>
