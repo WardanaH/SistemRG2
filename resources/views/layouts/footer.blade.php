2d4667b51931224046cd8ed064148b3a02f78fe9
WardanaH <rizkiwardana10@gmail.com>
2026-01-21 20:02:26 +0700

    update notif and footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,29 +1,29 @@
-<footer class="footer-area mt-3">
+<footer class="footer-area fixed-bottom bg-light">
     <div class="footer d-flex justify-content-between align-items-center px-3">
 
         <!-- COPYRIGHT KIRI -->
         <div class="footer-left">
             <div class="pull-right hidden-xs">
-                <b>Version</b> 1.0
+                <b style="color: black;">Version 1.0</b>
             </div>
-            <strong>Developped by <a href="https://www.instagram.com/runcode.id/" target="_blank">runcode</a>.</strong> &copy; 2018
+            <strong style="color: black;">Developped by <a href="https://www.instagram.com/runcode.id/" target="_blank" style="color: black;">runcode</a>.  &copy; 2018 </strong style="color: black;">
         </div>
 
         <!-- COPYRIGHT TENGAH -->
-        <div class="footer-center text-center">
-            <a href="https://www.instagram.com/wrdxxzz/" target="_blank">Wardana</a> |
-            <a href="https://www.instagram.com/diahmfr_/" target="_blank">Diah</a> |
-            <a href="https://www.instagram.com/iamfinethankyoundsinn/" target="_blank">Mujayhid</a>
+        <div class="footer-center text-center" style="color: black;">
+            <a href="https://www.instagram.com/wrdxxzz/" target="_blank"style="color: black;">Wardana</a> |
+            <a href="https://www.instagram.com/diahmfr_/" target="_blank"style="color: black;">Diah</a> |
+            <a href="https://www.instagram.com/iamfinethankyoundsinn/" target="_blank"style="color: black;">Mujayhid</a>
             &copy; Tim PKL Politala 2025–2026 Version 2.0
         </div>
 
         <!-- COPYRIGHT KANAN -->
-        <div class="footer-right d-flex flex-column align-items-end">
+        <div class="footer-right d-flex flex-column align-items-end"style="color: black;">
             <div>
-                Designed &amp; Developed by <a href="#" target="_blank">Quixkit</a> 2019
+                Designed &amp; Developed by <a href="#" target="_blank" style="color: black;">Quixkit</a> 2019
             </div>
             <div>
-                Distributed by <a href="https://themewagon.com/" target="_blank">Themewagon</a>
+                Distributed by <a href="https://themewagon.com/" target="_blank" style="color: black;">Themewagon</a>
             </div>
         </div>
 
